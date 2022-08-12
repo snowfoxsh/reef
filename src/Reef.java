9efe103dcb7bf89f83955c11b1017a00187a36f7
stormy482 <storm1surge@gmail.com>
2022-08-11 23:11:00 -0700

    [UNI] autoloads images in src

--- src/Reef.java
+++ src/Reef.java
@@ -1,11 +1,14 @@
 import processing.core.PApplet;
 import processing.core.PImage;
 
+import java.io.File;
+import java.util.HashMap;
+
 public class Reef extends PApplet {
     public static void main(String[] args) { PApplet.main("Reef"); }
 
     private final String res = "res/";
-
+    private HashMap<String, PImage> imgDb = new HashMap<>();
     Entity star;
 
     @Override
@@ -15,9 +18,10 @@ public class Reef extends PApplet {
 
     @Override
     public void setup() {
+        loadRes();
         frameRate(15);
 
-        PImage image = loadImage(res + "Star.png");
+        PImage image = imgDb.get("Star.png");
         star = new Entity(image, 400, 200, 70, 70);
         star.drawEntity(this);
     }
@@ -29,5 +33,17 @@ public class Reef extends PApplet {
         }
 
         star.drawEntity(this);
+    }
+
+    private void loadRes() {
+        File resDir = new File("src/res");
+        File[] files = resDir.listFiles();
+        for (File img : files) {
+            String name = img.getName();
+            if (name.endsWith(".png") || name.endsWith(".jpg")) {
+                System.out.println(img.getAbsolutePath());
+                imgDb.put(name, loadImage(img.getPath()));
+            }
+        }
     }
 }
